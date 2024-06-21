817b1e4987706d18ab2631d3bcd9d4b438b84987
Vinci Lee <95361587+Dobbinci@users.noreply.github.com>
2024-06-21 18:59:58 +0900

    Update motion_router.py

--- app/routers/motion_router.py
+++ app/routers/motion_router.py
@@ -24,23 +24,3 @@ async def get_drawing_with_motion(
     if not gif_path.is_file():
         return {"error": "gif not found"}
     return FileResponse(gif_path)
-
-
-@motion.post("/test")
-async def get_multipart_file(
-    image: UploadFile = File(...),
-    motion: str = Form(...)
-):
-    # image_data = await image.read() 
-    # nparr = np.frombuffer(image_data, np.uint8)  
-    # img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-    img = await img_to_np(image)
-
-    convert_to_GIF(img=img, char_anno_dir="output")
-
-    gif_path = Path("output/video.gif")
-
-    if not gif_path.is_file():
-        return {"error": "gif not found on the server"}
-    return FileResponse(gif_path)
